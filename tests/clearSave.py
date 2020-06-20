a0d5d6f09ba332cd2383f7042c02ee568f041534
Elephant34 <51174423+Elephant34@users.noreply.github.com>
2020-06-20 14:10:25 +0100

    Update clearSave.py

--- tests/clearSave.py
+++ tests/clearSave.py
@@ -3,11 +3,13 @@ import pathlib
 save_file = pathlib.Path("assets/data/worldSave.json")
 zone_dict = pathlib.Path("assets/data/zones/")
 
-if save_file.exists():
+try:
     save_file.unlink()
 
-for file in zone_dict.rglob("*"):
-    file.unlink()
-zone_dict.rmdir()
+    for file in zone_dict.rglob("*"):
+        file.unlink()
+    zone_dict.rmdir()
+except FileNotFoundError:
+    pass
 
 print("Game save files deleted")
